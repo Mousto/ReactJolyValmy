0ee72a6a28d0207d0668d27700623d3818898f69
Mousto <bediaraye-w@yahoo.fr>
2022-09-02 18:18:41 +0200

    création de styles et gestion d'affichage en cas erreur de conenxion =>  modif login.js et login.scss

--- src/Components/pages/Login.js
+++ src/Components/pages/Login.js
@@ -50,54 +50,63 @@ function Login(){
 	};
   const styles = {
     borderColor: isError ? 'red' : '', 
+    color: isError ? 'red' : '', 
   }
   return(
     <React.StrictMode>
-      <div className="div-connexion-form d-flex justify-content-center align-items-center mx-auto mt-5">
+      <div className="div-connexion-form  justify-content-center      align-items-center mx-auto mt-5 p-3">
         <form>
           <h3>Connexion</h3>
-          <div className="mb-3">
-            <label>Votre nom</label>
-            <input
-              style={styles}
-              type="text"
-              name="username"
-              className="form-control"
-              placeholder="Entrer le nom"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label>Mot de passe</label>
-            <input
-              style={styles}
-              type="password"
-              name="password"
-              className="form-control"
-              placeholder="Votre mot de passe"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-3">
-            <div className="custom-control custom-checkbox">
+          {
+            isError && 
+            <div className="div-erreur-connexion mb-3 p-2" style={styles}>
+              Veuillez entrer le nom d'utilisateur et le mot de passe correctes pour un compte personnel. Noter que les deux champs peuvent être sensibles à la casse.
+              </div>
+            }
+          <div className="div-champs-connexion">
+            <div className="mb-3">
+              <label>Votre nom</label>
               <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customCheck1"
+                /* style={styles} */
+                type="text"
+                name="username"
+                className="form-control"
+                placeholder="Entrer le nom"
+                onChange={handleChange}
               />
-              <label className="custom-control-label mx-1" htmlFor="customCheck1">
-                Souviens toi de moi
-              </label>
             </div>
+            <div className="mb-3">
+              <label>Mot de passe</label>
+              <input
+                /* style={styles} */
+                type="password"
+                name="password"
+                className="form-control"
+                placeholder="Votre mot de passe"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label className="custom-control-label mx-1" htmlFor="customCheck1">
+                  Souviens toi de moi
+                </label>
+              </div>
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+                Submit
+              </button>
+            </div>
+            <p className="forgot-password text-right mt-2">
+                Oublié <a href="#">password?</a>
+            </p>
           </div>
-          <div className="d-grid">
-            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
-              Submit
-            </button>
-          </div>
-          <p className="forgot-password text-right mt-2">
-              Oublié <a href="#">password?</a>
-          </p>
         </form>
        {/*  {isError && <Message 
                       variant='danger' 
